6ed60b77cffa68f260dd1930429c5534ccd19c98
Ojongseok <whdtjr6889@naver.com>
2026-07-25 00:10:27 +0900

    [refactor] #243: 벨 아이콘 벡터로 교체

--- feature/notification/impl/src/main/kotlin/com/neki/android/feature/notification/impl/component/NotificationEmptyContent.kt
+++ feature/notification/impl/src/main/kotlin/com/neki/android/feature/notification/impl/component/NotificationEmptyContent.kt
@@ -1,16 +1,18 @@
 package com.neki.android.feature.notification.impl.component
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import com.neki.android.core.designsystem.ComponentPreview
 import com.neki.android.core.designsystem.R
@@ -25,9 +27,10 @@ internal fun NotificationEmptyContent(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
-        Image(
+        Icon(
             modifier = Modifier.size(72.dp),
-            painter = painterResource(R.drawable.image_bell_fill),
+            imageVector = ImageVector.vectorResource(R.drawable.icon_bell_fill),
+            tint = Color.Unspecified,
             contentDescription = null,
         )
         Text(
