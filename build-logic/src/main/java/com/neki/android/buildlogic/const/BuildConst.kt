4be9deb09b1edcc2e58eafb5e4373536b03c31dd
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2026-08-17 19:05:17 +0900

    [chore] v1.3.5 버전 정보 업데이트

--- build-logic/src/main/java/com/neki/android/buildlogic/const/BuildConst.kt
+++ build-logic/src/main/java/com/neki/android/buildlogic/const/BuildConst.kt
@@ -5,8 +5,8 @@ import org.gradle.api.JavaVersion
 object BuildConst {
     const val APPLICATION_ID = "com.neki.android"
 
-    const val VERSION_CODE = 28
-    const val VERSION_NAME = "1.3.4"
+    const val VERSION_CODE = 30
+    const val VERSION_NAME = "1.3.5"
 
     const val MIN_SDK = 29
     const val TARGET_SDK = 36
